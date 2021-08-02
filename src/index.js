1db20fb18084f6b44c70f61c84d0920a0011a775
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-03 00:12:17 +0900

    ✨ UPDATE: draw result

--- src/index.js
+++ src/index.js
@@ -21,3 +21,20 @@ const user$ = fromEvent(document.getElementById("search"), "keyup").pipe(
 user$.subscribe((value) => {
   console.log("서버로부터 받은 검색 결과", value);
 });
+
+const $layer = document.getElementById("suggestLayer");
+
+function drawLayer(items) {
+  $layer.innerHTML = items
+    .map((user) => {
+      return `
+      <li class="user">
+      <img src="${user.avatar_url}" width="50px" height="50px" />
+      <p><a href="${user.html_url}" target="_blank">${user.login}</a></p>
+      </li>
+      `;
+    })
+    .join("");
+}
+
+user$.subscribe((v) => drawLayer(v.items));
