3138e4b552f124a3af7a1f90d73878b2101568bf
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-02 23:54:58 +0900

    ✨ NEW: github API call

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import { fromEvent } from "rxjs";
+import { from, fromEvent } from "rxjs";
 import { map } from "rxjs/operators";
 
 const keyup$ = fromEvent(document.getElementById("search"), "keyup").pipe(
   map((event) => event.target.value)
 );
 keyup$.subscribe((event) => console.log(`#search 에 입력된 값: ${event}`));
+
+//
+const request$ = from(
+  fetch(`https://api.github.com/search/users?q=yoophi`).then((res) =>
+    res.json()
+  )
+);
+request$.subscribe((json) => {
+  console.log("서버에서 전달 받은 값");
+  console.log({ json });
+});
+
+//
+import { ajax } from "rxjs/ajax";
+ajax
+  .getJSON("https://api.github.com/search/users?q=yoophi")
+  .subscribe((json) => {
+    console.log("서버에서 전달 받은 값");
+    console.log({ json });
+  });
