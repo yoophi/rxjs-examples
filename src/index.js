e1b90485747bffc0997bfd8bc8f9cfb2089518c6
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-03 00:00:48 +0900

    ✨ NEW: fetch API from user input

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-import { from, fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, mergeMap } from "rxjs/operators";
 
-const keyup$ = fromEvent(document.getElementById("search"), "keyup").pipe(
-  map((event) => event.target.value)
-);
-keyup$.subscribe((event) => console.log(`#search 에 입력된 값: ${event}`));
-
-//
-const request$ = from(
-  fetch(`https://api.github.com/search/users?q=yoophi`).then((res) =>
-    res.json()
+const user$ = fromEvent(document.getElementById("search"), "keyup").pipe(
+  map((event) => event.target.value),
+  mergeMap((query) =>
+    ajax.getJSON(`https://api.github.com/search/users?q=${query}`)
   )
 );
-request$.subscribe((json) => {
-  console.log("서버에서 전달 받은 값");
-  console.log({ json });
-});
 
-//
-import { ajax } from "rxjs/ajax";
-ajax
-  .getJSON("https://api.github.com/search/users?q=yoophi")
-  .subscribe((json) => {
-    console.log("서버에서 전달 받은 값");
-    console.log({ json });
-  });
+user$.subscribe((value) => {
+  console.log("서버로부터 받은 검색 결과", value);
+});
