a2abb3482addbaf4d273c05c99136da8497bfc26
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-02 23:49:24 +0900

    ✨ UPDATE: map to extract event.target.value

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
 
-const keyup$ = fromEvent(document.getElementById("search"), "keyup");
-keyup$.subscribe((event) =>
-  console.log(`사용자 입력의 KeyboardEvent: ${event}`)
+const keyup$ = fromEvent(document.getElementById("search"), "keyup").pipe(
+  map((event) => event.target.value)
 );
+keyup$.subscribe((event) => console.log(`#search 에 입력된 값: ${event}`));
