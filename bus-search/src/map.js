73d90c707aa028c027466147439bb372b44f0744
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-20 23:22:04 +0900

    ✨ NEW: show markers

--- bus-search/src/map.js
+++ bus-search/src/map.js
@@ -39,7 +39,10 @@ export default class Map {
   constructor($map) {
     this.naverMap = createNaverMap($map);
 
-    const station$ = this.createDragend$().pipe(this.mapStation);
+    const station$ = this.createDragend$().pipe(
+      this.mapStation,
+      this.manageMarker.bind(this)
+    );
 
     station$.subscribe(console.log);
   }
@@ -53,12 +56,37 @@ export default class Map {
     );
   }
 
+  createMarker(name, x, y) {
+    return new naver.maps.Marker({
+      map: this.naverMap,
+      title: name,
+      position: new naver.maps.LatLng(y, x),
+    });
+  }
+
   mapStation(coord$) {
     return coord$.pipe(
       switchMap((coord) =>
         ajax.getJSON(`/station/around/${coord.longitude}/${coord.latitude}`)
       ),
       hadnleAjax("busStationAroundList")
+    );
+  }
+
+  manageMarker(station$) {
+    return station$.pipe(
+      map((stations) =>
+        stations.map((station) => {
+          const marker = this.createMarker(
+            station.stationName,
+            station.x,
+            station.y
+          );
+          marker.setOptions("id", station.stationId);
+          marker.setOptions("name", station.stationName);
+          return marker;
+        })
+      )
     );
   }
 }
