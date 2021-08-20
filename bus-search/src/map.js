e5f3c02efbf0e577f4f3fc05b3cd13c4d4b416d2
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-20 23:56:46 +0900

    ✨ NEW: show bus station infoWindow

--- bus-search/src/map.js
+++ bus-search/src/map.js
@@ -1,16 +1,30 @@
-import { fromEvent } from "rxjs";
+import { combineLatest, of, fromEvent } from "rxjs";
 import { ajax } from "rxjs/ajax";
-import { map, switchMap } from "rxjs/operators";
+import { map, mergeMap, switchMap } from "rxjs/operators";
 
 function createNaverMap($map) {
   return new naver.maps.Map($map, {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 11,
+    // center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 14,
     minZoom: 6,
   });
 }
 
-export function hadnleAjax(prop) {
+function createNaverInfoWindow() {
+  return new naver.maps.InfoWindow();
+}
+
+function getBuesType(name) {
+  if (/^광역/.test(name)) {
+    return "yellow";
+  } else if (/^직행/.test(name)) {
+    return "red";
+  } else {
+    return "";
+  }
+}
+
+export function handleAjax(prop) {
   return (obs$) =>
     obs$.pipe(
       map((jsonRes) => {
@@ -38,13 +52,26 @@ export function hadnleAjax(prop) {
 export default class Map {
   constructor($map) {
     this.naverMap = createNaverMap($map);
+    this.infowindow = createNaverInfoWindow();
 
     const station$ = this.createDragend$().pipe(
       this.mapStation,
-      this.manageMarker.bind(this)
+      this.manageMarker.bind(this),
+      this.mapMarkerClick,
+      this.mapBus
     );
 
-    station$.subscribe(console.log);
+    station$.subscribe(({ markerInfo, buses }) => {
+      if (this.isOpenInfoWindow(markerInfo.position)) {
+        this.openInfoWindow(
+          markerInfo.marker,
+          markerInfo.position,
+          this.render(buses, markerInfo)
+        );
+      } else {
+        this.closeInfoWindow();
+      }
+    });
   }
 
   createDragend$() {
@@ -69,7 +96,7 @@ export default class Map {
       switchMap((coord) =>
         ajax.getJSON(`/station/around/${coord.longitude}/${coord.latitude}`)
       ),
-      hadnleAjax("busStationAroundList")
+      handleAjax("busStationAroundList")
     );
   }
 
@@ -88,5 +115,71 @@ export default class Map {
         })
       )
     );
+  }
+
+  mapMarkerClick(marker$) {
+    return marker$.pipe(
+      mergeMap((marker) => fromEvent(marker, "click")),
+      map(({ overlay }) => ({
+        marker: overlay,
+        position: overlay.getPosition(),
+        id: overlay.getOptions("id"),
+        name: overlay.getOptions("name"),
+      }))
+    );
+  }
+
+  mapBus(markerInfo$) {
+    return markerInfo$.pipe(
+      switchMap((markerInfo) => {
+        const marker$ = of(markerInfo);
+        const bus$ = ajax
+          .getJSON(`/bus/pass/station/${markerInfo.id}`)
+          .pipe(handleAjax("busRouteList"));
+        return combineLatest(marker$, bus$, (marker, buses) => ({
+          buses,
+          markerInfo,
+        }));
+      })
+    );
+  }
+
+  isOpenInfoWindow(position) {
+    return !(
+      position.equals(this.infowindow.getPosition()) && this.infowindow.getMap()
+    );
+  }
+
+  closeInfoWindow() {
+    this.infowindow.close();
+  }
+
+  openInfoWindow(marker, position, content) {
+    this.naverMap.panTo(position, { duration: 30 });
+    this.infowindow.setContent(content);
+    this.infowindow.open(this.naverMap, marker);
+  }
+
+  render(buses, { name }) {
+    const list = buses
+      .map(
+        (bus) => `
+        <dd>
+          <a href="#${bus.routeId}_${bus.routeName}">
+            <strong>${bus.routeName}</strong> 
+            <span>${bus.regionName}</span> 
+            <span class="type ${getBuesType(bus.routeTypeName)}">${
+          bus.routeTypeName
+        }</span>
+          </a>
+        </dd>`
+      )
+      .join("");
+
+    return `
+      <dl class="bus-routes">
+        <dt><strong>${name}</strong></dt>
+        ${list}
+      </dl>`;
   }
 }
