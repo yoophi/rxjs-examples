9461f1bff279b96e848b2528fd5cf7a7d510a446
Pyunghyuk Yoo <yoophi@gmail.com>
2021-08-20 22:40:40 +0900

    ✨ NEW: get bus stations

--- bus-search/src/map.js
+++ bus-search/src/map.js
@@ -1,3 +1,7 @@
+import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, switchMap } from "rxjs/operators";
+
 function createNaverMap($map) {
   return new naver.maps.Map($map, {
     center: new naver.maps.LatLng(37.3595704, 127.105399),
@@ -9,5 +13,26 @@ function createNaverMap($map) {
 export default class Map {
   constructor($map) {
     this.naverMap = createNaverMap($map);
+
+    const station$ = this.createDragend$().pipe(this.mapStation);
+
+    station$.subscribe(console.log);
+  }
+
+  createDragend$() {
+    return fromEvent(this.naverMap, "dragend").pipe(
+      map(({ coord }) => ({
+        latitude: coord.y,
+        longitude: coord.x,
+      }))
+    );
+  }
+
+  mapStation(coord$) {
+    return coord$.pipe(
+      switchMap((coord) =>
+        ajax.getJSON(`/station/around/${coord.longitude}/${coord.latitude}`)
+      )
+    );
   }
 }
